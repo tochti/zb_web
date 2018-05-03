36e7df4dd075ff9cb2b3785f5cf917382563440d
Tim Tochtermann <tim.tochtermann@aaronprojects.de>
2018-05-03 16:30:14 +0200

    Fix BackenCondition module

--- src/BackendCondition.tsx
+++ src/BackendCondition.tsx
@@ -1,45 +1,48 @@
-import React from "react";
-import produce from "immer";
-import { connect } from "react-redux";
-import AppState from "./state";
+import React from 'react';
+import produce from 'immer';
+import { connect } from 'react-redux';
+import AppState from './state';
 
 export enum BackendCondition {
-  OK,
-  Error
+    OK,
+    Error,
 }
 
-export const UPDATE_BACKEND_STATE = "UPDATE_BACKEND_STATE";
+export const UPDATE_BACKEND_STATE = 'UPDATE_BACKEND_STATE';
 export type UPDATE_BACKEND_STATE = typeof UPDATE_BACKEND_STATE;
 
 export interface UpdateBackendState {
-  type: UPDATE_BACKEND_STATE;
-  state: BackendCondition;
+    type: UPDATE_BACKEND_STATE;
+    state: BackendCondition;
 }
 
 export const updateBackendState = (
-  state: BackendCondition
+    state: BackendCondition
 ): UpdateBackendState => ({
-  type: UPDATE_BACKEND_STATE,
-  state: state
+    type: UPDATE_BACKEND_STATE,
+    state: state,
 });
 
-export const reducer = (state: BackendCondition, action: UpdateBackendState) =>
-  produce(state, draft => {
-    if (action.type == UPDATE_BACKEND_STATE) {
-      draft = action.state;
-    }
-  });
+export const reducer = (
+    state: BackendCondition | undefined,
+    action: UpdateBackendState
+) =>
+    produce(state, draft => {
+        if (action.type == UPDATE_BACKEND_STATE) {
+            draft = action.state;
+        }
+    });
 
 const BackendConditionComponent = (props: { condition: BackendCondition }) => {
-  return (
-    <div>
-      <p>{BackendCondition.OK ? "OK" : "Error"}</p>
-    </div>
-  );
+    return (
+        <div>
+            <p>{BackendCondition.OK ? 'OK' : 'Error'}</p>
+        </div>
+    );
 };
 
 const mapStateToProps = (state: AppState) => ({
-  condition: state.backendCondition
+    condition: state.backendCondition,
 });
 
 export default connect(mapStateToProps)(BackendConditionComponent);
