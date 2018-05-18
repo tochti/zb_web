2fa9011b851bad9ee7855406517a1055b9bfc0bd
Tim Tochtermann <tim.tochtermann@aaronprojects.de>
2018-05-18 22:42:56 +0200

    Add redux-logger

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxLogger from 'redux-logger';
 import AppState from './appState';
 import rootReducer from './rootReducer';
 
-const store = createStore<AppState>(rootReducer);
+const store = createStore<AppState>(rootReducer, applyMiddleware(reduxLogger));
 
 export default store;
